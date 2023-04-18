272e087173f66e1cc3e7b9eb7b2006e18f836a68
vijit kumar <vijitkumar699@gmail.com>
2023-04-18 21:03:39 +0530

    new commit

--- script.js
+++ script.js
@@ -9,3 +9,49 @@ slide1.addEventListener("click",(e)=>{
         slide1.classList.toggle("translate-x-[-100%]")
     }
 })
+
+// Initialization for ES Users
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 4000); // Change image every 2 seconds
+} 
+
+
+var slidePosition1 = 1;
+SlideShow1(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow1(slidePosition1 += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow1(slidePosition1 = n);
+}
+
+function SlideShow1(n) {
+    var i;
+    var slides = document.getElementsByClassName("Containers");
+    var circles = document.getElementsByClassName("dots");
+    if (n > slides.length) {slidePosition1 = 1}
+    if (n < 1) {slidePosition1 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < circles.length; i++) {
+        circles[i].className = circles[i].className.replace(" enable", "");
+    }
+    slides[slidePosition1-1].style.display = "block";
+    circles[slidePosition1-1].className += " enable";
+  } 
